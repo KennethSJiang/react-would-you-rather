429c58ac2651d3a5e6eddfe89e78c6f7d38c2cac
Kenneth Jiang <kenneth@pelotoncycle.com>
2018-05-08 22:10:29 -0400

    refactor error page

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,30 +1,14 @@
 import React, {Component} from 'react'
-import {Redirect} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
-class ErrorPage extends Component{
-  state={
-    toHome: false
-  }
-
-  handleClick = (e) =>{
-    e.preventDefault()
-    this.setState(()=>({
-      toHome: true
-    }))
-  }
-
-  render(){
-    if(this.state.toHome){
-      return <Redirect to='/' />
-    }
-
-    return(
-      <div className="error-container">
-        <h1>404</h1>
-        <p className="return">Question does not exist. Back to </p><div className='replying-to' onClick={(e) => this.handleClick(e)}>Home Dashboard</div>
-      </div>
-    )
-  }
+const ErrorPage = () =>{
+  return(
+    <div className="error-container">
+      <h1>404</h1>
+      <p className="return">Question does not exist. Back to </p><Link to='/'>Home</Link>
+    </div>
+  )
 }
+
 
 export default ErrorPage
